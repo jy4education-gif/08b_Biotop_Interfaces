98fb11c28bee1150027a154d5a552bd088a10a58
Ji-Yong <jy4education@gmail.com>
2026-02-02 12:43:45 +0100

    Update App.java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,6 +5,8 @@ import com.cc.java.bees.HoneyBee;
 import com.cc.java.bees.Queen;
 import com.cc.java.bees.Worker;
 import com.cc.java.birds.Bird;
+import com.cc.java.interfaces.IFeathers;
+import com.cc.java.interfaces.IFlyable;
 
 public class App {
     
@@ -13,27 +15,32 @@ public class App {
         Queen queen = new Queen();
         Worker worker = new Worker();
         Drone drone = new Drone();   
+        Bird bird = new Bird();
 
         pollObj(queen);
         pollObj(worker);
         pollObj(drone);
-
-        Bird bird = new Bird();
         pollObj(bird);
      
     }
 
-    private static void pollObj(HoneyBee obj){
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("-------------------");
+
+    private static void pollObj(IFlyable obj) {
+    output(obj.fly());
+
+    // Prüfung auf Bienen-Verhalten
+    if (obj instanceof HoneyBee) {
+        HoneyBee bee = (HoneyBee) obj;
+        output(bee.doYourJob());
     }
 
-    // überadene Methode
-    private static void pollObj(Bird obj){
-        output(obj.hasFeathers());
-        output(obj.fly());
-        output("-------------------");
+    // Prüfung auf Feder-Eigenschaft (Das hat nur der Bird)
+    if (obj instanceof IFeathers) {
+        IFeathers birdWithFeathers = (IFeathers) obj;
+        output(birdWithFeathers.hasFeathers());
+    }
+
+    output("-------------------");
     }
 
 
